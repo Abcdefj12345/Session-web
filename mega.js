3ff68b4869a2c57dfd3005725f67709b0858a06f
CyberRushModz0 <queenrashumd0715@gmail.com>
2025-06-23 15:50:18 +0530

    Add files via upload

--- mega.js
+++ mega.js
@@ -1,19 +1,11 @@
 
-//_____ _    _ _      _    __  __ ____  
- // / ____| |  | | |    / \  |  \/  |  _ \ 
- //| (___ | |  | | |   / _ \ | |\/| | | | |
- // \___ \| |  | | |  / ___ \| |  | | |_| |
- // ____) | |__| | |_/ /   \ \_|  |_|____/ 
- //|_____/ \____/|_____/     \_\          
- 
-//             S U L A - M D
 
 import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'sulakshamadara4@gmail.com', // Replace with your Mega email
-    password: 'Sulakshamd011222@ms', // Replace with your Mega password
+    email: 'queenrashumd0715@gmail.com', // Replace with your Mega email
+    password: 'Rashu#1234567890', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
