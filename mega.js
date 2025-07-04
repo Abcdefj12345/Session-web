6e79b853b9f0a854d81aa3842c7191f6b3263790
CyberRushModz0 <queenrashumd0715@gmail.com>
2025-07-05 00:39:33 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -4,8 +4,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'rashupanel@gmail.com', // Replace with your Mega email
-    password: 'Rashu#123456789012', // Replace with your Mega password
+    email: 'nipunpanel@gmail.com', // Replace with your Mega email
+    password: 'Rashu#1234567890', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
