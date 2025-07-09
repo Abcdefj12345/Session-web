120f243d3d29b216532df8c1d1b8ea4d43a90139
CyberRushModz0 <queenrashumd0715@gmail.com>
2025-07-09 09:20:31 +0530

    Add files via upload

--- mega.js
+++ mega.js
@@ -1,11 +1,10 @@
 
-
 import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'sadeeshatharumin@gmail.com', // Replace with your Mega email
-    password: 'larasessionid@2', // Replace with your Mega password
+    email: 'darkwebagent096@gmail.com', // Replace with your Mega email
+    password: 'Darknetofficialgh@@2144', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
